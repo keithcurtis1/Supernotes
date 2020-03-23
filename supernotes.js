ae86b1ee61a911a4d6702835e906983fae96227d
keithcurtis1 <keithcurtis@gmail.com>
2020-03-23 15:50:30 -0700

    Update supernotes.js

--- supernotes.js
+++ supernotes.js
@@ -1,5 +1,5 @@
 on('ready', () => {
-    // Set these values for template of choice and toggle the value of sendToPlayers if you want a fotter button to pass the message on to players
+    // Set these values for template of choice and toggle the value of sendToPlayers if you want a footer button to pass the message on to players
     const template = 'npcaction';
     const title = 'rname';
     const theText = 'description';
@@ -9,8 +9,7 @@ on('ready', () => {
 
     on('chat:message', function(msg) {
         if ('api' === msg.type && msg.content.match(/^!(gm|pc)note/) && playerIsGM(msg.playerid)) {
-            let match = msg.content.match(/^!gmnote-(.*)$/),
-            regex;
+            let match = msg.content.match(/^!gmnote-(.*)$/);
 
             //define command                     
             let command = msg.content.split(/\s+--/)[0];
@@ -35,6 +34,7 @@ on('ready', () => {
             }
 
 
+            let regex;
             if (match && match[1]) {
                 regex = new RegExp(`^${match[1]}`, 'i');
             }
@@ -44,46 +44,49 @@ on('ready', () => {
             if ((option === 'bio')||(option === 'charnote')) {
                 let suboption = (option === 'charnote') ? 'gmnotes' : 'bio';
 
-                _.chain(msg.selected)
-                .map(s => getObj('graphic', s._id))
-                .reject(_.isUndefined)
-                .map(t => getObj('character', t.get('represents')))
-                .reject(_.isUndefined)
-                .each(c => c.get(suboption, (val) => {
-                    if (null !== val && 'null' !== val && val.length > 0) {
-                        if (regex) {
-                            message = _.filter(
-                                decodeUnicode(val).split(/(?:[\n\r]+|<br\/?>)/),
-                                (l) => regex.test(l.replace(/<[^>]*>/g, ''))
-                            ).join('\r');
-                        } else {
-                            message = decodeUnicode(val);
-                        }
-                        whom = c.get('name');
-                    }
-                }));
-            } else {
-                _.chain(msg.selected)
-                .map(s => getObj('graphic', s._id))
-                .reject(_.isUndefined)
-                .reject((o) => o.get('gmnotes').length === 0)
-                .each(o => {
-                    if (regex) {
-                        message = _.filter(decodeURIComponent(decodeUnicode(o.get('gmnotes'))).split(/(?:[\n\r]+|<br\/?>)/), (l) => regex.test(l)).join('\r');
-                    } else {
-                        message = decodeURIComponent(decodeUnicode(o.get('gmnotes')));
-                    }
-                    whom = o.get('name');
+                (msg.selected || [])
+                    .map(o=>getObj('graphic',o._id))
+                    .filter(g=>undefined !== g)
+                    .map(t => getObj('character', t.get('represents')))
+                    .filter(c=>undefined !== c)
+                    .forEach(c => c.get(suboption, (val) => {
+                        if (null !== val && 'null' !== val && val.length > 0) {
+                            if (regex) {
+                                message = _.filter(
+                                    decodeUnicode(val).split(/(?:[\n\r]+|<br\/?>)/),
+                                    (l) => regex.test(l.replace(/<[^>]*>/g, ''))
+                                ).join('\r');
+                            } else {
+                                message = decodeUnicode(val);
+                            }
+                            whom = c.get('name');
+                            //Sends the final message
+                            sendChat(whom, messagePrefix + '&{template:' + template + '}{{' + title + '=' + whom + '}} {{' + theText + '=' + message + playerButton + '}}');
 
-                });
+                        }
+                    }));
+            } else {
+                (msg.selected || [])
+                    .map(o=>getObj('graphic',o._id))
+                    .filter(g=>undefined !== g)
+                    .filter((o) => o.get('gmnotes').length > 0)
+                    .forEach(o => {
+                        if (regex) {
+                            message = _.filter(decodeURIComponent(decodeUnicode(o.get('gmnotes'))).split(/(?:[\n\r]+|<br\/?>)/), (l) => regex.test(l)).join('\r');
+                        } else {
+                            message = decodeURIComponent(decodeUnicode(o.get('gmnotes')));
+                        }
+                        whom = o.get('name');
+
+                    });
+                //Sends the final message
+                sendChat(whom, messagePrefix + '&{template:' + template + '}{{' + title + '=' + whom + '}} {{' + theText + '=' + message + playerButton + '}}');
 
             }
 
-            //Sends the final message
-            sendChat(whom, messagePrefix + '&{template:' + template + '}{{' + title + '=' + whom + '}} {{' + theText + '=' + message + playerButton + '}}');
 
             [
-                `### REPORT###'}`,
+                `### REPORT###`,
                 `THE MESSAGE =${message}`,
                 `command = ${command}`,
                 `option = ${option}`,
